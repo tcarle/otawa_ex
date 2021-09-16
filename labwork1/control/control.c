92ea883a0c3d5e11fd2bac70529168d278e2ced5
tcarle <thomas.carle@irit.fr>
2021-09-16 19:48:03 +0200

    Ready for lab

---
+++ labwork1/control/control.c
@@ -0,0 +1,35 @@
+int main();
+
+void _exit(int r);
+/*void _exit(int r) {
+	while(1);
+}*/
+
+void _start(void) {
+	int r = main();
+	_exit(r);
+}
+
+
+int one(void) {
+	return 1;
+}
+
+int zero(void) {
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	int (*f)(void);
+
+	/* exit example */
+	if(argc == 0)
+		_exit(1);
+
+	/* branch example */
+	if(argc <= 1)
+		f = one;
+	else
+		f = zero;
+	return f();
+}
